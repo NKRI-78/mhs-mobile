039788a01c5685aa2b914eb563abe527d42e3fbb
rahmadfani <rahmadfani46@gmail.com>
2024-08-02 15:55:29 +0700

    done

--- lib/repositories/home_repository/home_repository.dart
+++ lib/repositories/home_repository/home_repository.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:mhs_mobile/misc/api_url.dart';
 import 'package:mhs_mobile/misc/client.dart';
+import 'package:mhs_mobile/misc/pagination.dart';
+import 'package:mhs_mobile/repositories/home_repository/models/news_model.dart';
 
 class HomeRepository {
   Uri get bannerUri => Uri.parse('${MyApi.baseUrl}/api/v1/banner');
@@ -26,7 +28,7 @@ class HomeRepository {
     }
   }
 
-  Future<void> getNews() async {
+  Future<PaginationModel<NewsModel>> getNews() async {
     try {
       var res = await MyClient().get(newUri);
 
@@ -34,10 +36,16 @@ class HomeRepository {
 
       final json = jsonDecode(res.body);
       if (res.statusCode == 200) {
-        return;
+        var pagination = Pagination.fromJson(json['data']);
+        var list = (json['data']['data'] as List)
+            .map((e) => NewsModel.fromJson(e))
+            .toList();
+        return PaginationModel<NewsModel>(pagination: pagination, list: list);
       }
       if (res.statusCode == 400) {
         throw json['message'] ?? "Terjadi kesalahan";
+      } else {
+        throw "Error";
       }
     } catch (e) {
       rethrow;
