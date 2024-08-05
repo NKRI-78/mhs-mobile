3f480737ce665c5ffff6f7a8e864a0af1148e1eb
rahmadfani <rahmadfani46@gmail.com>
2024-08-05 16:31:10 +0700

    done

--- lib/modules/home/widgets/banners_widget.dart
+++ lib/modules/home/widgets/banners_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mhs_mobile/modules/home/bloc/home_bloc.dart';
+import 'package:shimmer/shimmer.dart';
 
 class BannersWidget extends StatelessWidget {
   const BannersWidget({super.key});
@@ -10,17 +11,43 @@ class BannersWidget extends StatelessWidget {
     return BlocBuilder<HomeBloc, HomeState>(
       buildWhen: (previous, current) => previous.banners != current.banners,
       builder: (context, state) {
-        if (state.banners.isEmpty) return const SizedBox.shrink();
-        return Container(
-          height: MediaQuery.of(context).size.width * .4,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
+        if (state.banners.isEmpty) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: Shimmer.fromColors(
+              baseColor: Colors.grey[300]!,
+              highlightColor: Colors.grey[100]!,
+              child: Container(
+                height: MediaQuery.of(context).size.width * .4,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white,
+                ),
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                ),
+              ),
+            ),
+          );
+        }
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: Container(
+            clipBehavior: Clip.antiAlias,
+            height: MediaQuery.of(context).size.width * .4,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: const EdgeInsets.symmetric(
+              horizontal: 32,
+            ),
+            child: Image.network(
+              state.banners.first.bannerLink ?? '',
+              fit: BoxFit.cover,
+            ),
           ),
-          margin: const EdgeInsets.symmetric(
-            horizontal: 32,
-          ),
-          child: Image.network(state.banners.first.bannerLink ?? ''),
         );
       },
     );
