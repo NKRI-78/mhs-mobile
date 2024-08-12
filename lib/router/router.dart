80958bdeb3a26e2328c0bf29eeda1fc326d258fe
rahmadfani <rahmadfani46@gmail.com>
2024-08-12 16:44:32 +0700

    done

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -6,7 +6,7 @@ class MyRouter {
   static GoRouter init(AppBloc app) => GoRouter(
         routes: $appRoutes,
         initialLocation: app.state.alreadyShowBeginingTour
-            // ? HomeRoute().location
+            //? HomeRoute().location
             ? WaitingPaymentRoute(id: "ORD-MHS-1723451275492").location
             : BeginingTourRoute().location,
       );
