49e34d4035698e69ac7c3c686cf335d4c1593219
rahmadfani <rahmadfani46@gmail.com>
2024-08-12 12:52:51 +0700

    on progress waiting payment page

---
+++ lib/modules/waiting_payment/view/waiting_payment_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WaitingPaymentPage extends StatelessWidget {
+  const WaitingPaymentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const WaitingPaymentView();
+  }
+}
+
+class WaitingPaymentView extends StatelessWidget {
+  const WaitingPaymentView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
