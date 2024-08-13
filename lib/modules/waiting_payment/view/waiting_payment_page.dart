caed8c4a7a506baa85f3833d99a90b01e91b8a2c
rahmadfani <rahmadfani46@gmail.com>
2024-08-13 13:44:13 +0700

    pembayaran ui

--- lib/modules/waiting_payment/view/waiting_payment_page.dart
+++ lib/modules/waiting_payment/view/waiting_payment_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:mhs_mobile/misc/theme.dart';
 import 'package:mhs_mobile/modules/waiting_payment/cubit/waiting_payment_cubit.dart';
 
 class WaitingPaymentPage extends StatelessWidget {
@@ -25,12 +28,195 @@ class WaitingPaymentView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Pembayaran"),
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+        title: const Text(
+          "Pembayaran",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: primaryColor,
       ),
-      body: const Column(
-        children: [
-          Text("Status"),
-        ],
+      body: SizedBox.expand(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Status",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const Text(
+                  'Menunggu Pemayaran',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.red,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const Text(
+                  "Batas akhir pembayaran",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const Text(
+                  'Juli, 08 april 2024',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const Divider(
+                  color: Colors.grey,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const Text(
+                  "Nomor Virtual Account",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Expanded(
+                      child: Text(
+                        '1372627736626255',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: const Row(
+                        children: [
+                          Text(
+                            'Salin',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: primaryColor,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Icon(
+                            Iconsax.copy,
+                            color: primaryColor,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                const Text(
+                  "Total Pembayaran",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Expanded(
+                      child: Text(
+                        'Rp 200.000',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: const Row(
+                        children: [
+                          Text(
+                            'Salin',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: primaryColor,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Icon(
+                            Iconsax.copy,
+                            color: primaryColor,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: primaryColor.withOpacity(.3),
+                      border: Border.all(
+                        color: primaryColor,
+                      ),
+                      borderRadius: BorderRadius.circular(6)),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
+                  ),
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Iconsax.info_circle,
+                        color: primaryColor,
+                        size: 35,
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Expanded(
+                        child: Text(
+                            'Tidak disarankan transfer Virtual Account dari bank selain yang dipilih.'),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const Divider(
+                  color: Colors.grey,
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
